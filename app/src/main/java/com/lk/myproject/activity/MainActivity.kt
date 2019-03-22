5dd581c05488dec4c7add9a5de869ac512ad0de0
lvkang <lvkang@yy.com>
2019-03-22 10:18:57 +0800

    progress

--- app/src/main/java/com/lk/myproject/activity/MainActivity.kt
+++ app/src/main/java/com/lk/myproject/activity/MainActivity.kt
@@ -54,6 +54,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun test() {
-        startActivity(Intent(this, PermissionTestActivity::class.java))
+        var test = "移动OA上线会议计划于2012年05月19日9点在102会议室准时召开，请各位准时参加会议!房间安静的身份辣椒粉杜蕾斯打附加赛科技大厦防雷接地萨拉会计分录快点撒解放路口的就是大家是否缴费的卢卡斯解放东路卡萨！"
+        myText.text = toSBC(test)
+        //startActivity(Intent(this, PermissionTestActivity::class.java))
+    }
+
+    /**
+     * textview 文字参差不齐问题
+     */
+    fun toSBC(input: String): String {
+        val c = input.toCharArray()
+        for (i in c.indices) {
+            if (c[i] == ' ') {
+                c[i] = '\u3000'
+            } else if (c[i] < '\u007f') {
+                c[i] = (c[i].toInt() + 65248).toChar()
+            }
+        }
+        return String(c)
     }
 }
