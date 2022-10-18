d72ebb975744d51f6cb610745febccad49d25eb9
lvkang <lvkang@joyy.com>
2022-10-18 15:17:24 +0800

    room数据库 数据加密

--- app/src/main/java/com/lk/myproject/room/encrypt/StringConverter.java
+++ app/src/main/java/com/lk/myproject/room/encrypt/StringConverter.java
@@ -1,16 +1,11 @@
 package com.lk.myproject.room.encrypt;
 
-import android.util.Log;
-
 import androidx.room.TypeConverter;
 
 public class StringConverter {
     @TypeConverter
     public static StringEntry revertStringConverter(String value) {
-        Log.d("AndroidR", "revertStringConverter : " + value);
         String real = AccountEncrypt.getAccountEncrypt().getRealEncryptContent(value);
-        Log.d("AndroidR", "revertStringConverter real: " +
-                AccountEncrypt.getAccountEncrypt().decodeContent(real));
         return new StringEntry(AccountEncrypt.getAccountEncrypt().decodeContent(real));
     }
 
